2863bbd68c2d3c341fed97d9ee35af563fc8f3b0
Lord Nighton <lordnightonnik@gmail.com>
2019-07-10 10:52:33 +0300

    Scheduling enabled for job

--- src/main/java/com/example/batch/BatchConfig.java
+++ src/main/java/com/example/batch/BatchConfig.java
@@ -1,23 +1,44 @@
 package com.example.batch;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableBatchProcessing
+@EnableScheduling
 public class BatchConfig extends DefaultBatchConfigurer {
 
+    @Autowired
+    private JobBuilderFactory jobBuilders;
+
+    @Autowired
+    private StepBuilderFactory stepBuilders;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Scheduled(fixedRate = 5000)
+    public void run() throws Exception {
+        JobExecution execution = jobLauncher.run(
+                helloWorldJob(),
+                new JobParametersBuilder().addLong("uniqueness", System.nanoTime()).toJobParameters()
+        );
+    }
+
     @Bean
-    public Job helloWorldJob(JobBuilderFactory jobBuilders,
-                             StepBuilderFactory stepBuilders) {
+    public Job helloWorldJob() {
         return jobBuilders.get("helloWorldJob")
                 .start(printHelloWorldStep(stepBuilders))
                 .build();
